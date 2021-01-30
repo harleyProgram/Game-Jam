e6c968ac4eee77a7d522330988a6dad91fd51d39
harleyProgram <harleygamesdesigner@gmail.com>
2021-01-30 14:58:58 +0800

    Basic formation of stage one rooms
    I have created a base version of the formation for the generated rooms. We can either create the room we like and have it generate from a list or make the rooms generate later on.

---
+++ Global Game Jame 20210/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public List<GameObject> lostItemsList = new List<GameObject>();
+    
+    public List<GameObject> roomSpawners = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+    
+    
+    
+    
+}
