2d08530593c9820083a0bf2bd76b5dd234cbac85
harleyProgram <harleygamesdesigner@gmail.com>
2021-01-31 02:23:13 +0800

    Added  new package
    Added GUI package
    - Working on trainsition
    - Added time clock moving to real time.
    - Added lost condition, win condition.
    - Added Main menu
    - Added information in main menu

--- Global Game Jame 20210/Assets/Scripts/GameHandler.cs
+++ Global Game Jame 20210/Assets/Scripts/GameHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Random = System.Random;
 
@@ -12,15 +13,33 @@ public class GameHandler : MonoBehaviour
 
     private GenerateRooms _generateRooms;
 
+    [SerializeField] private Canvas gameOverCanvas;
+    [SerializeField] private TMP_Text endText;
+
+    public Transform lostItem;
+    
+    public float currentTimeLeft = 0.0f;
+    private float maxTime = 90.0f;
+
     private void Awake()
     {
         _generateRooms = FindObjectOfType<GenerateRooms>();
-
+        
     }
 
     void Start()
     {
         SpawnLostItem();
+        Time.timeScale = 1.0f;
+    }
+
+    private void Update()
+    {
+        currentTimeLeft += Time.deltaTime;
+
+        if (currentTimeLeft < maxTime) return;
+        
+        GameOver();
     }
 
     private void SpawnLostItem()
@@ -28,7 +47,8 @@ public class GameHandler : MonoBehaviour
         int rng = UnityEngine.Random.Range(0, lostItemsList.Count);
 
         Instantiate(lostItemsList[rng], LostSpot().position, Quaternion.identity);
-        
+
+        lostItem = lostItemsList[rng].transform;
         Debug.Log("look for: " + lostItemsList[rng].name);
 
     }
@@ -42,6 +62,14 @@ public class GameHandler : MonoBehaviour
         int rngSpot = UnityEngine.Random.Range(0, hiddenSpots.hiddenLocations.Count);
         
         return hiddenSpots.hiddenLocations[rngSpot].transform;
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0.0f;
+        gameOverCanvas.enabled = true;
+        endText.text = "You had to leave without your " + lostItem.name;
+
     }
     
     
