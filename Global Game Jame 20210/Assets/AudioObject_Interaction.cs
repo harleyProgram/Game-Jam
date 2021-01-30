9da569316895be47d7cbe0ec91ba9ae87b95dcf7
Garalax <gareth.griffiths250@gmail.com>
2021-01-30 20:44:12 +0800

    Clocks and Mixing
    - Added Clock noises
    - Mixed music
    - Mixed sound distance

---
+++ Global Game Jame 20210/Assets/AudioObject_Interaction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Player_Controller.Interaction_types
+{
+    public class AudioObject_Interaction : InteractableObject
+    {
+        public AudioSource sources;
+        public override void Interact()
+        {
+            if (sources.enabled == false)
+            {
+                sources.enabled = true;
+            }
+            else
+            {
+                sources.enabled = false;
+            }
+        }
+    }
+}
